24fc331cffa06d9e4e72841e6a4e8eb292b8f6a8
jogging <446734796@qq.com>
2017-04-01 17:30:45 +0800

    exesice

---
+++ src/com/wq/alg0401m/SortEx.java
@@ -0,0 +1,25 @@
+package com.wq.alg0401m;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortEx {
+
+	public static void main(String[] args) {
+	
+		 int[][] a={{1,3},{4,2},{2,4},{1,9}};
+		 int b[][]={{1,5,2,8},{5,9,10,-3},{2,7,-5,-18}};
+		 System.out.println(""+b[1][1]);
+//		Arrays.sort(a,new Comparator<int[]>(){
+//
+//			@Override
+//			public int compare(int[] o1, int[] o2) {
+//				return  o1[1]-o2[2];
+//			}
+//			
+//		});
+//		System.out.println(a.toString());
+		
+	}
+
+}
