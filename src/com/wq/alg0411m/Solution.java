4447f14006687c79226de63e53e81fb6ed9ed929
jogging520 <446734796@qq.com>
2017-04-11 09:37:30 +0800

    0411 commit from baiyin

---
+++ src/com/wq/alg0411m/Solution.java
@@ -0,0 +1,13 @@
+package com.wq.alg0411m;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
+        List<List<Integer>> resList=new ArrayList<List<Integer>>();
+        //思路：矩阵中每一位进行上下左右查找，如果有0，标记为1，如果没有找出上下左右中最大的（1），然后＋1
+        
+        return resList;
+    }
+}
