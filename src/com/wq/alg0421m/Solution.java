b3f8de6635dda255776579debbfbd245df66f2c9
jogging <446734796@qq.com>
2017-04-21 16:00:56 +0800

    0420commit  map +bfs's <stack>

---
+++ src/com/wq/alg0421m/Solution.java
@@ -0,0 +1,76 @@
+package com.wq.alg0421m;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Administrator
+ * @332
+ * 2017年4月21日 上午8:52:37   
+ * Example 1:
+	tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+	Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
+	Example 2:
+	tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+	Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
+ */
+public class Solution {
+	public List<String> findItinerary(String[][] tickets) {
+   	 List<String> resList=new ArrayList<String>();
+   	 if(tickets==null||tickets.length==0||tickets[0].length==0) return resList;
+   	 Map<String,List<String>> map=new HashMap<String,List<String>>();
+   	 for(int i=0;i<tickets.length;i++){//build a from to'sLIst map
+			 List<String> list=new ArrayList<String>();
+   		 if(!map.containsKey(tickets[i][0])){
+   			   list.add(tickets[i][1]);
+   	           map.put(tickets[i][0], list);		 
+   		 }
+   		 else{
+   		    list=map.get(tickets[i][0]);
+   		    list.add(tickets[i][1]);
+   		    map.replace(tickets[i][0], list);
+   		 }
+   	 }
+   	 
+   	 //go around the map take the list 
+   	 String start="JFK";
+   	 System.out.println("map:"+map);
+
+   	 System.out.println(map.get(start).size()!=0);
+   	 while(map!=null&&map.get(start).size()!=0){
+   		 String toPlace=sortlist(map,start);
+   		 resList.add(start);
+   		 start=toPlace; 
+   	 }
+   	  resList.add(start);
+   	  return resList;
+   	 
+   }
+
+	private String sortlist(Map<String, List<String>> map, String start) {
+		 List<String> list=new ArrayList<String>();
+
+		 Collections.sort(map.get(start),new Comparator<String>(){
+
+			@Override
+			public int compare(String o1, String o2) {
+				// TODO Auto-generated method stub
+				return o1.compareTo(o2);
+			}
+			
+		 });
+		list=map.get(start);
+       String res=list.remove(0);
+       map.replace(start,list);
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
---
+++ src/com/wq/alg0421m/Solution2.java
@@ -0,0 +1,55 @@
+package com.wq.alg0421m;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Stack;
+
+/**
+ * @author Administrator
+ * @332
+ * 2017��4��21�� ����8:52:37   
+ * Example 1:
+	tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+	Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
+	Example 2:
+	tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+	Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
+	�����ǣ�1.map���� 2.dfs ����
+ */
+
+ public class Solution2 {
+	public List<String> findItinerary(String[][] tickets) {
+   	 List<String> resList=new ArrayList<String>();
+   	 if(tickets==null||tickets.length==0||tickets[0].length==0) return resList;
+   	 Map<String,PriorityQueue<String>> map=new HashMap<String,PriorityQueue<String>>();
+   	 for(int i=0;i<tickets.length;i++){//build a from to'sLIst map
+   		 if(!map.containsKey(tickets[i][0])){
+   	           map.put(tickets[i][0], new PriorityQueue<String>());		 
+   		 }
+   		
+   		    map.get(tickets[i][0]).add(tickets[i][1]);
+   		 
+   	 }
+   	 
+   	//DFS
+   	 Stack<String> stack=new Stack<String>();
+   	 stack.push("JFK");
+   	 while(!stack.isEmpty()){
+   		 String key=stack.peek();
+   		 if(map.containsKey(key)&&!map.get(key).isEmpty()){
+   			 stack.push(map.get(key).poll());
+   		 }else{
+   			 resList.add(0, stack.pop());
+   		 }
+   	 }
+   	 return resList;
+   	 
+   }
+
+	
+}
